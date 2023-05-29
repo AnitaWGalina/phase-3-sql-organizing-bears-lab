87dcbfe789a0487ba3e1d873813072535544dcd1
anitagalina <anitagalina21@gmail.com>
2023-05-29 22:41:18 +0300

    Completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (Mr.Chocolate,20,M,blue,introvert,True);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (Rowdy,21,M,green,introvert,True);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (Tabitha,22,F,lilac,extrovert,True);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (Sergeant Brown,90,M,red,introvert,True);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (Melissa,23,F,white,extrovert,True);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (Grinch,25,M,maroon,introvert,True);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (Wendy,27,F,lavender,extrovert,True);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (NULL,30,M,yellow,introvert,True);
